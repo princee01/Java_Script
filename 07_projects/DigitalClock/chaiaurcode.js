945ffd3209edea7021a90c8a3f49743208029d87
Prince Upadhyay <upadhyayprince946@gmail.com>
2025-06-24 16:47:17 +0530

    Delete 07_projects/DigitalClock directory

--- 07_projects/DigitalClock/chaiaurcode.js
+++
@@ -1,9 +0,0 @@
-const clock = document.querySelector("#clock");
-// or
-// const clock=document.getElementById('clock')
-
-
-setInterval(function () {
-    let date = new Date;
-    clock.innerHTML=date.toLocaleTimeString()
-},1000)
