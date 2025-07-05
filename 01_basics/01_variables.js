7e735a21db29208729b34fbee4c7f29a7190da24
Prince Upadhyay <upadhyayprince946@gmail.com>
2025-07-05 16:11:01 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,14 @@
-//value written in constant cant be changed one written
+//1.value written in constant cant be changed one written
+/*
+2.var:
+-->This is the oldest keyword for variable declaration in JavaScript.
+-->Variables declared with var are function-scoped or globally-scoped.
+ This means they are accessible throughout the function in which they are declared, 
+ or globally if declared outside any function.
+-->var allows for redeclaration and updating of a variable's value.
+*/
+//3.let
+//-->let is block scoped means a variable declared with let is only accessible within the specific block
 const accoundId = 14253
 let accountEmail = "prince@998"
 var accountPassword = "1234"
