785841943456fd8b22c55b432b8c9b6f5388e1f4
princeee <upadhyayprince946@gmail.com>
2025-09-08 19:42:42 +0530

    completed clculator project

---
+++ practice/3_calculator/script.js
@@ -0,0 +1,28 @@
+const input=document.querySelector("input")
+const buttons = document.querySelectorAll("button")
+
+const arr = Array.from(buttons)
+console.log(arr);
+
+
+arr.forEach(button => {
+    button.addEventListener("click",(e) => {
+        if (e.target.innerHTML == "=") {
+            input.value= eval(input.value)
+             
+        }
+        else if (e.target.innerHTML == "AC") {
+            
+            input.value = "";
+        }
+        else if (e.target.innerHTML == "del") {
+            input.value=input.value.slice(0, -1);
+            
+        } else {
+            input.value+=e.target.innerHTML
+        }
+            
+        
+    })
+    
+})
